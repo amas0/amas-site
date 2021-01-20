1ca42dafee0ee7e2835ada198227adc0acb00f28
amas <andrew.mascioli1@gmail.com>
2021-01-20 17:43:18 -0500

    Add some contact info About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,31 +5,34 @@ import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 
 const AboutPage = () => {
-	return (
-		<StaticQuery
-		query={graphql`
-			query SiteTitleQuery2 {
-				site {
-					siteMetadata {
-						title
-					}
-				}
-			}
-		`}
-		render={(data) => (
-			<Layout>
-				<SEO title="About"/>
-				<p>
-					Hey, I'm Andrew. {data.site.siteMetadata.title} is my
-					blog about math, games, data, combinations of those, and
-					whatever else may strike my personal interest. I suppose
-					that means this blog could be about anything. Reader 
-					beware.
-				</p>
-			</Layout>
-		)}
-		/>
-	)
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery2 {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+        }
+      `}
+      render={(data) => (
+        <Layout>
+          <SEO title="About" />
+          <p>
+            Hey, I'm Andrew. {data.site.siteMetadata.title} is my blog about
+            math, games, data, combinations of those, and whatever else may
+            strike my personal interest. I suppose that means this blog could be
+            about anything. Reader beware.
+          </p>
+          <p>
+            You can check me on <a href="https://twitter.com/_amas0">Twitter</a>
+            , <a href="https://github.com/amas0">Github</a> or email me directly.
+          </p>
+        </Layout>
+      )}
+    />
+  );
 };
 
 export default AboutPage;
