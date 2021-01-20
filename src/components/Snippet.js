eb07b862937e644db609325a4ebed71c81d1fd9f
amas <andrew.mascioli1@gmail.com>
2021-01-20 18:04:34 -0500

    Update snippet for link highlighting

--- src/components/Snippet.js
+++ src/components/Snippet.js
@@ -2,24 +2,31 @@ import React from "react";
 import { Link } from "gatsby";
 import { jsx, NavLink } from "theme-ui";
 
-function Snippet({frontmatter}) {
-	return (
-		<Link to={frontmatter.path} style={{ textDecoration: "none" }}>
-			<div style={{ 
-				fontSize: "24px", 
-				fontWeight: "bold" 
-			}}>
-				{frontmatter.title}
-			</div>
-			<div style={{
-				fontSize: "18px",
-				display: "inline",
-				color: "#2A2A2A"
-			}}>
-			{frontmatter.date} - {frontmatter.summary}
-			</div>
-		</Link>
-	);
+function Snippet({ frontmatter }) {
+  return (
+    <Link to={frontmatter.path} style={{ textDecoration: "none" }}>
+      <NavLink>
+        <div
+          style={{
+            fontSize: "24px",
+            fontWeight: "bold",
+          }}
+        >
+          {frontmatter.title}
+        </div>
+        <div
+          style={{
+			fontSize: "18px",
+			fontWeight: "normal",
+            display: "inline",
+            color: "#2A2A2A",
+          }}
+        >
+          {frontmatter.date} - {frontmatter.summary}
+        </div>
+      </NavLink>
+    </Link>
+  );
 }
 
 export default Snippet;
