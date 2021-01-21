e8208741d8876596213f11ebd837efb711dcac67
amas <andrew.mascioli1@gmail.com>
2021-01-20 19:02:10 -0500

    Update about with thanks and fix staticquery

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,39 +1,38 @@
 import React from "react";
-import { StaticQuery, graphql } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
 
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 
-const AboutPage = () => {
-  return (
-    <StaticQuery
-      query={graphql`
-        query SiteTitleQuery2 {
-          site {
-            siteMetadata {
-              title
-            }
-          }
+export default function AboutPage() {
+  const { site } = useStaticQuery(graphql`
+    query AboutPageQuery {
+      site {
+        siteMetadata {
+          title
         }
-      `}
-      render={(data) => (
-        <Layout>
-          <SEO title="About" />
-          <p>
-            Hey, I'm Andrew. {data.site.siteMetadata.title} is my blog about
-            math, games, data, combinations of those, and whatever else may
-            strike my personal interest. I suppose that means this blog could be
-            about anything. Reader beware.
-          </p>
-          <p>
-            You can check me out on{" "}
-            <a href="https://twitter.com/_amas0">Twitter</a>,{" "}
-            <a href="https://github.com/amas0">Github</a> or email me directly.
-          </p>
-        </Layout>
-      )}
-    />
+      }
+    }
+  `);
+  return (
+    <Layout>
+      <SEO title="About" />
+      <p>
+        Hey, I'm Andrew. {site.siteMetadata.title} is my blog about math, games,
+        data, combinations of those, and whatever else may strike my personal
+        interest. I suppose that means this blog could be about anything. Reader
+        beware.
+      </p>
+      <p>
+        You can check me out on <a href="https://twitter.com/_amas0">Twitter</a>
+        , <a href="https://github.com/amas0">Github</a>, or email me directly.
+      </p>
+      <p>
+        This site was built with <a href="https://www.gatsbyjs.com/">Gatsby</a>{" "}
+        and deployed with <a href="https://www.netlify.com/">netlify</a>.
+        Special thanks to <a href="https://github.com/zeevosec">zeevosec</a> for
+        providing starter code and guidance on building the site.
+      </p>
+    </Layout>
   );
-};
-
-export default AboutPage;
+}
