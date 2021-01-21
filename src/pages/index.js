e38f215174e486753136f1111e93260bf044e94f
amas <andrew.mascioli1@gmail.com>
2021-01-21 17:39:49 -0500

    Fix homepage title

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,19 +8,24 @@ import Masthead from "../components/Masthead";
 import SEO from "../components/SEO";
 
 import { Vega } from "react-vega";
+import { Helmet } from "react-helmet";
 
 function Index({ data }) {
   console.log(Vega);
   return (
     <Layout>
-      <SEO title="Home" />
-      <Masthead/>
-      <hr sx={{ height: 1 }}/>
+      <Masthead />
+      <hr sx={{ height: 1 }} />
       {data.allMdx.edges.map(({ node }) => (
         <div key={node.frontmatter.path}>
           <HomePagePost node={node}></HomePagePost>
         </div>
       ))}
+      <Helmet
+        title="Analytics Check"
+        titleTemplate="%s"
+        description="...can I roll Perception instead?"
+      />
     </Layout>
   );
 }
